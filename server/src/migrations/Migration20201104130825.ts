87e6337cb108273eb2e1f2a79601e6a415b05ece
abba5 <abbastrick@gmail.com>
2020-11-05 15:33:38 +0530

    change orm

--- server/src/migrations/Migration20201104130825.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20201104130825 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('alter table `user` add `email` text not null;');
-  }
-
-}
